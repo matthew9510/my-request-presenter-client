8eeee1bed34b9f4042b11469dd7c55f6aa62e184
Matthew Hess <matthew9510@gmail.com>
2020-02-18 15:43:23 -0800

    request logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component'
+import { MakeRequestComponent } from './components/make-request/make-request.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'authentication', pathMatch: 'full' },
+  { path:'authentication', component: AuthenticationComponent },
+  { path: 'make-request', component: MakeRequestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AuthenticationComponent } from './components/authentication/authentication.component'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { MakeRequestComponent } from './components/make-request/make-request.component';
+
+import { UserService } from './services/user.service'
+import { RequestsService } from './services/requests.service'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    MakeRequestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    AmplifyAngularModule,
   ],
-  providers: [],
+  providers: [AmplifyService, UserService, RequestsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
