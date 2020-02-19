f0c21c75b57c007f72c7b28de42d306397527f7d
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-02-19 15:05:28 -0800

    setting up requests component

--- src/app/components/request-details/request-details.component.ts
+++ src/app/components/request-details/request-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-request-details',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-details.component.scss']
 })
 export class RequestDetailsComponent implements OnInit {
+  @Input() artist: string;
+  @Input() song: string;
+  @Input() amount: number;
+  @Input() status: string;
+  @Input() createdOn: string;
 
-  constructor() { }
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
   }
+
+  get isSmallScreen() {
+    return this.breakpointObserver.isMatched('(max-width: 450px)');
+  }
+
+  get isLargeScreen() {
+    return this.breakpointObserver.isMatched('(min-width: 700px)')
+  }
+
 
 }
