bff4d275bbc654cc91131f1b65b4bdb05d535d40
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-02-19 14:45:10 -0800

    adding requests component to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RequestsComponent } from './components/requests/requests.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'requests', pathMatch: 'full' },
+  { path: 'requests', component: RequestsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
