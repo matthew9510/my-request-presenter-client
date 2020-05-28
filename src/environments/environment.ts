ea3e05e674ed28d8599562553445e2081300016b
Peter Horton <phorton@softstackfactory.com>
2020-05-28 14:10:08 -0700

    Update environment urls (#138)
    * Show end event of message
    
    * Altered the events, requests, and venues url for the environment file. Also removed unnecessary urls
    
    Co-authored-by: Matthew Hess <matthew9510@gmail.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,14 @@
 export const environment = {
   production: false,
   baseUrl: "https://develop.d2875do098zx8f.amplifyapp.com",
-  authUrl: "",
   eventsUrl:
-    "https://tlsvnj7msi.execute-api.us-west-2.amazonaws.com/dev/events",
-  imagesUrl:
-    "https://90bx9hlja5.execute-api.us-west-2.amazonaws.com/dev/images",
-  queueUrl: "https://u8pn7ho2md.execute-api.us-west-2.amazonaws.com/dev/queue",
+    "https://0n4g9hsewd.execute-api.us-west-2.amazonaws.com/dev/events",
   requestsUrl:
-    "https://8vgvs38e0m.execute-api.us-west-2.amazonaws.com/dev/requests",
+    "https://05wqrtg3r2.execute-api.us-west-2.amazonaws.com/dev/requests",
   requesterUrl:
     "https://npfjwp3yrd.execute-api.us-west-2.amazonaws.com/dev/requester",
-  transactionsUrl:
-    "https://hndjafk1sl.execute-api.us-west-2.amazonaws.com/dev/transactions",
   venuesUrl:
-    "https://4gw3k61sti.execute-api.us-west-2.amazonaws.com/dev/venues",
+    "https://6wa1c5s4gd.execute-api.us-west-2.amazonaws.com/dev/venues",
   performersUrl:
     "https://ts1drgp3g3.execute-api.us-west-2.amazonaws.com/dev/performers",
 };
