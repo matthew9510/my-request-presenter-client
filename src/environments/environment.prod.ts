c7b943cb6acf118fae6c5577bae3201ce072858a
Peter Horton <phorton@softstackfactory.com>
2020-06-02 10:44:58 -0700

    updated production urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,16 @@ export const environment = {
   production: true,
   frontendUrl: "https://requester.ssflabs.com",
   eventsUrl:
-    "https://tlsvnj7msi.execute-api.us-west-2.amazonaws.com/dev/events",
+    "https://970enl1mz3.execute-api.us-west-2.amazonaws.com/prod/events",
   requestsUrl:
-    "https://8vgvs38e0m.execute-api.us-west-2.amazonaws.com/dev/requests",
+    "https://kwckpypns3.execute-api.us-west-2.amazonaws.com/prod/requests",
   requesterUrl:
-    "https://npfjwp3yrd.execute-api.us-west-2.amazonaws.com/dev/requester",
+    "https://ba9pwvwtga.execute-api.us-west-2.amazonaws.com/prod/requester",
 
   venuesUrl:
     "https://npbggtaft6.execute-api.us-west-2.amazonaws.com/prod/venues",
   performersUrl:
-    "https://ts1drgp3g3.execute-api.us-west-2.amazonaws.com/dev/performers",
+    "https://9qvk4ld314.execute-api.us-west-2.amazonaws.com/prod/performers",
   aws_project_region: "us-west-2",
   aws_cognito_region: "us-west-2",
   aws_user_pools_id: "us-west-2_2rPpXcag5",
