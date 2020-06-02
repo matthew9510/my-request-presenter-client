09cc8167b204d3ce616f1574e2e9d6d36cffb9fe
Peter Horton <phorton@softstackfactory.com>
2020-06-02 09:44:39 -0700

    updated prod env venues url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  frontendUrl: "https://develop.d2875do098zx8f.amplifyapp.com",
+  frontendUrl: "https://requester.ssflabs.com",
   eventsUrl:
     "https://tlsvnj7msi.execute-api.us-west-2.amazonaws.com/dev/events",
   requestsUrl:
     "https://8vgvs38e0m.execute-api.us-west-2.amazonaws.com/dev/requests",
   requesterUrl:
     "https://npfjwp3yrd.execute-api.us-west-2.amazonaws.com/dev/requester",
-  venuesUrl:
-    "https://4gw3k61sti.execute-api.us-west-2.amazonaws.com/dev/venues",
+
+  venuesUrl: "https://npbggtaft6.execute-api.us-west-2.amazonaws.com/prod",
   performersUrl:
     "https://ts1drgp3g3.execute-api.us-west-2.amazonaws.com/dev/performers",
   aws_project_region: "us-west-2",
