7a86098c0fccc936bed53143d196b08f8caf3697
Matt <matthew9510@gmail.com>
2020-07-07 13:42:50 -0700

    Initialization of stripe payment form component

---
+++ src/app/components/stripe-payment-form/stripe-payment-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-payment-form',
+  templateUrl: './stripe-payment-form.component.html',
+  styleUrls: ['./stripe-payment-form.component.scss']
+})
+export class StripePaymentFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
